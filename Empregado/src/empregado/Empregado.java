b5d60c56519a093d1320b2fa898539f2245a360b
Jorge Mota <Jorge Mota>
2017-03-31 20:01:16 +0100

    Continuação Ex Empregado...

--- Empregado/src/empregado/Empregado.java
+++ Empregado/src/empregado/Empregado.java
@@ -5,6 +5,7 @@
  */
 package empregado;
 
+import java.util.Calendar;
 import utilitarios.Data;
 import utilitarios.Tempo;
 
@@ -13,20 +14,17 @@ import utilitarios.Tempo;
  * @author Jorge Mota
  */
 public class Empregado {
-    
+
     String primeiroNome;
     String ultimoNome;
     Data dataContrato;
     Tempo horaEntrada;
     Tempo horaSaida;
-    
+
     private static final String PRIMEIRO_NOME_POR_OMISSAO = "Sem nome";
     private static final String ULTIMO_NOME_POR_OMISSAO = "sem ultimo nome";
-    
-    
-    
-    
-    public Empregado(String primeiroNome, String ultimoNome, Data dataContrato, Tempo horaEntrada, Tempo horaSaida){
+
+    public Empregado(String primeiroNome, String ultimoNome, Data dataContrato, Tempo horaEntrada, Tempo horaSaida) {
         this.primeiroNome = primeiroNome;
         this.ultimoNome = ultimoNome;
         this.dataContrato = new Data(dataContrato);
@@ -34,87 +32,85 @@ public class Empregado {
         this.horaSaida = new Tempo(horaSaida);
         
     /**
-     *Contrutor vazio
-     */
-    public Empregado() {
-    primeiroNome = PRIMEIRO_NOME_POR_OMISSAO;
-    ultimoNome = ULTIMO_NOME_POR_OMISSAO;
-    dataContrato = new Data(dataContrato);
-    horaEntrada = new Tempo(horaEntrada);
-    horaSaida = new Tempo(horaSaida);    
+    * Contrutor vazio
+    */
+    public Empregado(){
+        primeiroNome = PRIMEIRO_NOME_POR_OMISSAO;
+        ultimoNome = ULTIMO_NOME_POR_OMISSAO;
+        dataContrato = new Data(dataContrato);
+        horaEntrada = new Tempo(horaEntrada);
+        horaSaida = new Tempo(horaSaida);
     }
-    
-    
-    
-    public String getUltimoNome(){
-    return ultimoNome;
+
+    public String getUltimoNome() {
+        return ultimoNome;
     }
-    
-    public String getPrimeiroNome(){
-    return primeiroNome;
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
     }
-        
+
     /**
      *
      * @return dataContrato
-     */   
-    public Data getDataContrato(){
-    return dataContrato;    
+     */
+    public Data getDataContrato() {
+        return dataContrato;
     }
 
-    
-    public Tempo getHoraEntrada(){
-    return horaEntrada;
+    public Tempo getHoraEntrada() {
+        return horaEntrada;
     }
-    
-    
-    public Tempo getHoraSaida(){
-    return horaSaida;
-    }
-    
-    
-    public void setPrimeiroNome(String primeiroNome){
-    this.primeiroNome = primeiroNome;
-    }
-    
-    public void setUltimoNome(String ultimoNome){
-    this.ultimoNome = ultimoNome;
-    }
-    
-    
-    public void setHoraEntrada(Tempo horaEntrada){
-    this.horaEntrada = horaEntrada;
-    }
-    
-    public void setHoraSaida(Tempo horaSaida){
-    this.horaSaida = horaSaida;    
-    }
-    
-    
-    public Tempo getEmpregadoHorasTrabalhoDiario(Tempo horaEntrada, Tempo horaSaida){
-    float numeroHorasTrabalhoDiario = (gethoraSaida() - gethoraSaida()); 
-    return numeroHorasTrabalhoDiario;
-    }
-  
-  /**
-   * Calcular o número de horas de trabalho semanal de um empregado, 
-   * considerando que todos os empregados trabalham 5 dias por semana;
-   * @return 
-   */
-  public float calcularHorasTrabalho(){
-      float numeroHorasTrabalhoSemanal;
-      numeroHorasTrabalhoSemanal = (getEmpregadoHorasTrabalhoDiario()*5);   
-      return numeroHorasTrabalhoSemanal;
-      
-  }
 
-  // Determinar há quanto tempo o empregado foi contratado.
- public Tempo calcularTempoContrato(){
-     float tempoContrato;
-     tempoContrato = Data.dataAtual() - getDataContrato();
-     
- }
-  
-            
-            
+    public Tempo getHoraSaida() {
+        return horaSaida;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public void setUltimoNome(String ultimoNome) {
+        this.ultimoNome = ultimoNome;
+    }
+
+    public void setHoraEntrada(Tempo horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
+
+    public void setHoraSaida(Tempo horaSaida) {
+        this.horaSaida = horaSaida;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Empregado:%n Nome:%s;%n Sobrenome:%s;%n Data de contrato:%s;%n Hora de entrada:%s;%n Hora de saída:%s;%n",
+                primeiroNome, ultimoNome, dataContrato.toAnoMesDiaString(), horaEntrada, horaSaida);
+    }
+
+    /**
+     * Calcular o número de horas de trabalho semanal de um empregado,
+     * considerando que todos os empregados trabalham 5 dias por semana;
+     *
+     * @return
+     */
+    public float calcularHorasTrabalho() {
+        float numeroHorasTrabalhoSemanal;
+        numeroHorasTrabalhoSemanal = (horaEntrada.diferencaEmSegundos(horaSaida)) * 60 * 60 * 5;
+        return numeroHorasTrabalhoSemanal;
+    }
+
+    /**
+     * Determinar há quanto tempo o empregado foi contratado.
+     *
+     * @return tempoDecontrato - Nº de dias de contrato
+     */
+    public int calcularTempoContrato() {
+        int tempoDeContrato;
+        tempoDeContrato = (dataContrato.diferenca(Data.dataAtual()));
+        return tempoDeContrato;
+    }
+
 }
--- Empregado/src/empregado/TesteEmpregado.java
+++ Empregado/src/empregado/TesteEmpregado.java
@@ -21,4 +21,29 @@ public class TesteEmpregado {
         // TODO code application logic here
     }
     
+//a) Crie e visualize uma instância de Data para representar a data atual;
+    
+//b) Crie e visualize duas instâncias de Tempo;
+
+//c) Crie duas instâncias de Empregado usando para o efeito as instâncias de Data e de Tempo criadas anteriormente;
+
+//d) Teste se os atributos do tipo Data dos empregados criados possuem referências partilhadas;
+
+//e) Teste se os atributos do tipo Tempo dos empregados criados possuem referências partilhadas;
+
+//f) Altere o conteúdo do objeto Data e o conteúdo dos objetos Tempo inicialmente criados;
+
+//g) Visualize as instâncias de Data e de Tempo, bem como as instâncias de Empregado criadas;
+
+//h) Altere a data de contrato e as horas de entrada e de saída do segundo empregado;
+
+//i) Armazene as instâncias de Empregado criadas num contentor de objetos do tipo array;
+
+//j) Liste todos os empregados;
+
+//k) Liste o nome, o número de horas de trabalho por semana e a antiguidade de cada um dos empregados.
+    
+    
+    
+    
 }
