324c8a95e566c944e6628b8d4c2892f731f98867
Jorge Mota <Jorge Mota>
2017-03-31 19:16:48 +0100

    Continuação Ex Empregado

--- Empregado/src/empregado/Empregado.java
+++ Empregado/src/empregado/Empregado.java
@@ -20,6 +20,11 @@ public class Empregado {
     Tempo horaEntrada;
     Tempo horaSaida;
     
+    private static final String PRIMEIRO_NOME_POR_OMISSAO = "Sem nome";
+    private static final String ULTIMO_NOME_POR_OMISSAO = "sem ultimo nome";
+    
+    
+    
     
     public Empregado(String primeiroNome, String ultimoNome, Data dataContrato, Tempo horaEntrada, Tempo horaSaida){
         this.primeiroNome = primeiroNome;
@@ -28,8 +33,88 @@ public class Empregado {
         this.horaEntrada = new Tempo(horaEntrada);
         this.horaSaida = new Tempo(horaSaida);
         
-       
+    /**
+     *Contrutor vazio
+     */
+    public Empregado() {
+    primeiroNome = PRIMEIRO_NOME_POR_OMISSAO;
+    ultimoNome = ULTIMO_NOME_POR_OMISSAO;
+    dataContrato = new Data(dataContrato);
+    horaEntrada = new Tempo(horaEntrada);
+    horaSaida = new Tempo(horaSaida);    
     }
     
     
+    
+    public String getUltimoNome(){
+    return ultimoNome;
+    }
+    
+    public String getPrimeiroNome(){
+    return primeiroNome;
+    }
+        
+    /**
+     *
+     * @return dataContrato
+     */   
+    public Data getDataContrato(){
+    return dataContrato;    
+    }
+
+    
+    public Tempo getHoraEntrada(){
+    return horaEntrada;
+    }
+    
+    
+    public Tempo getHoraSaida(){
+    return horaSaida;
+    }
+    
+    
+    public void setPrimeiroNome(String primeiroNome){
+    this.primeiroNome = primeiroNome;
+    }
+    
+    public void setUltimoNome(String ultimoNome){
+    this.ultimoNome = ultimoNome;
+    }
+    
+    
+    public void setHoraEntrada(Tempo horaEntrada){
+    this.horaEntrada = horaEntrada;
+    }
+    
+    public void setHoraSaida(Tempo horaSaida){
+    this.horaSaida = horaSaida;    
+    }
+    
+    
+    public Tempo getEmpregadoHorasTrabalhoDiario(Tempo horaEntrada, Tempo horaSaida){
+    float numeroHorasTrabalhoDiario = (gethoraSaida() - gethoraSaida()); 
+    return numeroHorasTrabalhoDiario;
+    }
+  
+  /**
+   * Calcular o número de horas de trabalho semanal de um empregado, 
+   * considerando que todos os empregados trabalham 5 dias por semana;
+   * @return 
+   */
+  public float calcularHorasTrabalho(){
+      float numeroHorasTrabalhoSemanal;
+      numeroHorasTrabalhoSemanal = (getEmpregadoHorasTrabalhoDiario()*5);   
+      return numeroHorasTrabalhoSemanal;
+      
+  }
+
+  // Determinar há quanto tempo o empregado foi contratado.
+ public Tempo calcularTempoContrato(){
+     float tempoContrato;
+     tempoContrato = Data.dataAtual() - getDataContrato();
+     
+ }
+  
+            
+            
 }
